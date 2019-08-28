d2dd5c63c04a698dea697e72c69af61d94e1480b
Santiago <sagudecod97@gmail.com>
2019-08-28 17:22:58 +0000

    Created header holberton and function _putchar.c

---
+++ holberton.h
@@ -0,0 +1,28 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+char *get_Line(void);
+char **_strtok1(char *buff);
+char **_strtok2(char *path);
+char *_strcpy(char *dest, char *src);
+int _strLen(char *str);
+char *strcomp(char *envi);
+char *arrRet(char **env);
+char **cocaCommand(char *command, char **arrEnv);
+char **getPath(char **env);
+char *_strcat(char *dest, char *src);
+int forky(char **argum, char **path, char **env);
+int exitEnvBuilt(char **env, char *argum);
+int _strcmp(char *s1, char *s2);
+int nonInteractive(char **env);
+
+#endif
